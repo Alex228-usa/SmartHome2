15a23d69d94b1a3d44551da11bc2ffc050e62c06
Alikhan <58951356+Alex228-usa@users.noreply.github.com>
2024-01-19 13:49:30 +0600

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+* {
+  font-family: "Comfortaa", monospace;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content: center;
+  background-color: #BCC3BC;
+  padding: 10px;
+  height: 100vh;
+}
+</style>
